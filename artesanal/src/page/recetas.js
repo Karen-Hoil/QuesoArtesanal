6e63bbd23d9b976661246df686e8e7278f3bda40
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2023-11-01 09:15:04 -0600

    Cambios en cardRecetas y pagina recetas

--- artesanal/src/page/recetas.js
+++ artesanal/src/page/recetas.js
@@ -1,21 +1,23 @@
 import React from "react";
-import Sidebar from "../components/sidebar";
-import Navbar from "../components/navbar";
-import CardRecetas from "../components/cardRecetas";
+import Sidebar from '../components/sidebar'
+import Navbar from '../components/navbar'
+import CardRecetas from '../components/cardRecetas'
 
 function Recetas() {
   return (
     <>
-      <Sidebar></Sidebar>
-      <Navbar></Navbar>
-      <h2>Recetas</h2>
-      <div className="ml-1/6 grid grid-cols-2 gap-4">
-        <CardRecetas />
-        <CardRecetas />
-        <CardRecetas />
-        <CardRecetas />
-        <CardRecetas />
-        <CardRecetas />
+      <Sidebar />
+        <Navbar />
+      <div className="absolute left-[20%] ">
+        <h2 className="m-2">Recetas</h2>
+        <div className="grid grid-cols-3 gap-4">
+            <CardRecetas />
+            <CardRecetas />
+            <CardRecetas />
+            <CardRecetas />
+            <CardRecetas />
+            <CardRecetas />
+        </div>
       </div>
     </>
   );
