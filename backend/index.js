64a0b15bd863a3c93bf7c8d2011302269cbf710e
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2023-11-05 10:59:19 -0600

    APIs para recetas

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const usuarioRouter = require('./router/ususarioRouter');
+const recetasRouter = require('./router/RecetasRouter');
 
 const app = express();
 app.use(cors());
@@ -10,7 +11,7 @@ app.use(express.json());
 
 //Ruta al router
 app.use('/usuarios', usuarioRouter);
-
+app.use('/recetas', recetasRouter);
 
 app.listen(3001, () =>{
     console.log("API escuchando por el puerto 3001");
