c03e76e3da5ca1550b71ac01f5d2d6a34d47a341
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2023-11-05 10:57:56 -0600

    Hago funcional todo lo de recetas menos agregar

--- artesanal/src/App.js
+++ artesanal/src/App.js
@@ -27,9 +27,9 @@ function App() {
       element: <Recetas />
     },
     {
-      path: '/recetaInfo',
+      path: '/recetaInfo/:id_receta',
       element: <RecetaInfo />
-    },
+    },    
     {
       path:'/',
       element: <Login/>
--- artesanal/src/components/cardRecetas.js
+++ artesanal/src/components/cardRecetas.js
@@ -1,33 +1,16 @@
-// import React from "react";
-// import Col from "react-bootstrap/Col";
-// import Image from "react-bootstrap/Image";
 import Badge from "react-bootstrap/Badge";
-import Queso from "../img/queso.jpg";
-
-// function CardRecetas({ imagen, nombre, id }) {
-//   return (
-//     <Col xs={6} md={4}>
-//       <Image src={Queso} thumbnail />
-//       <h6>
-//         <Badge bg="secondary">Asadera chica</Badge>
-//       </h6>
-//     </Col>
-//   );
-// }
-
-// export default CardRecetas;
-
+// import Queso from "../img/queso.jpg";
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
 function CardRecetas({ imagen, nombre, id }) {
   return (
-    <Link to='/recetaInfo'>
+    <Link to={`/recetaInfo/${id}`}>
     <Card className="w-60 h-60">
-      <Card.Img variant="top" src={Queso} />
+      <Card.Img variant="top" src={imagen} />
       <Card.Body>
-        <Card.Title><Badge bg="secondary">Asadera chica</Badge></Card.Title>
+        <Card.Title><Badge bg="secondary">{nombre}</Badge></Card.Title>
       </Card.Body>
     </Card>
     </Link>
